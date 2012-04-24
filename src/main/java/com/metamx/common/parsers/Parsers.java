5659929892e6c1fa1be96d316c11565bae11cde3
xvrl <xavier@metamarkets.com>
2012-04-24 15:08:41 -0700

    add javadoc

--- src/main/java/com/metamx/common/parsers/Parsers.java
+++ src/main/java/com/metamx/common/parsers/Parsers.java
@@ -9,6 +9,11 @@ import java.util.Map;
 public class Parsers
 {
   public static <K, V> Function<String, Map<K, V>> toFunction(final Parser p) {
+
+    /**
+     * Creates a Function object wrapping the given parser.
+     * Parser inputs that throw an IOException are mapped to null.
+     */
     return new Function<String, Map<K, V>>() {
       @Override
       public Map<K, V> apply(String input) {
