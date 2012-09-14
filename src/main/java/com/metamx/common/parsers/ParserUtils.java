13537562c4a0d6746e6a3085ef1e1fae9403ab33
Jeffrey Wong <jeff@metamarkets.com>
2012-09-13 17:24:52 -0700

    MMX style issues

--- src/main/java/com/metamx/common/parsers/ParserUtils.java
+++ src/main/java/com/metamx/common/parsers/ParserUtils.java
@@ -76,15 +76,17 @@ public class ParserUtils
         String zone = (String) enuKeys.nextElement();
         float offset = Float.parseFloat(properties.getProperty(zone));
         int hours = (int) Math.floor(offset);
-        int minutes = (int) (60 * (offset - hours)) ;
+        int minutes = (int) (60 * (offset - hours));
         timezones.put(zone, DateTimeZone.forOffsetHoursMinutes(hours, minutes));
       }
-    } catch (FileNotFoundException e) {
+    }
+    catch (FileNotFoundException e) {
       throw new FormattedException.Builder()
           .withErrorCode(FormattedException.ErrorCode.FILE_NOT_FOUND)
           .withMessage("Could not find timezone configuration file - timezone.properties - in resource folder")
           .build();
-    } catch (IOException e) {
+    }
+    catch (IOException e) {
       throw new FormattedException.Builder()
           .withErrorCode(FormattedException.ErrorCode.IO_ERROR)
           .withMessage("Could not read timezone configuration file - timezone.properties - in resource folder")
@@ -93,7 +95,10 @@ public class ParserUtils
     return ParserUtils.createTimestampParser(format, timezones);
   }
 
-  public static Function<String, DateTime> createTimestampParser(final String format, final Map<String, DateTimeZone> timezones)
+  public static Function<String, DateTime> createTimestampParser(
+      final String format,
+      final Map<String, DateTimeZone> timezones
+  )
   {
     if (format.equalsIgnoreCase("iso")) {
       return new Function<String, DateTime>()
@@ -115,10 +120,12 @@ public class ParserUtils
           return new DateTime(Long.parseLong(input) * 1000);
         }
       };
-    } else if(format.equalsIgnoreCase("millis")) {
-      return new Function<String, DateTime>() {
+    } else if (format.equalsIgnoreCase("millis")) {
+      return new Function<String, DateTime>()
+      {
         @Override
-        public DateTime apply(String input) {
+        public DateTime apply(String input)
+        {
           Preconditions.checkArgument(input != null && !input.isEmpty(), "null timestamp");
           return new DateTime(Long.parseLong(input));
         }
@@ -127,7 +134,7 @@ public class ParserUtils
       try {
         Pattern pattern = Pattern.compile("[zQ]");
         Matcher matcher = pattern.matcher(format);
-        if(matcher.find()) {
+        if (matcher.find()) {
           return new Function<String, DateTime>()
           {
             @Override
@@ -137,8 +144,7 @@ public class ParserUtils
               return buildTimeStampParser(format, timezones, input).toFormatter().parseDateTime(input);
             }
           };
-        }
-        else {
+        } else {
           final DateTimeFormatter formatter = DateTimeFormat.forPattern(format);
           return new Function<String, DateTime>()
           {
@@ -172,33 +178,39 @@ public class ParserUtils
       String format,
       Map<String, DateTimeZone> timezones,
       String input
-  ) {
+  )
+  {
     DateTimeFormatterBuilder formatBuilder = new DateTimeFormatterBuilder();
     boolean insideLiteral = false;
     int parseablePatternStart = 0;
-    for(int i = 0; i < format.length(); i++) {
+    for (int i = 0; i < format.length(); i++) {
       char f = format.charAt(i);
-      if(f == '\'') insideLiteral = !insideLiteral;
-      if(f == 'z' && !insideLiteral) {
-        formatBuilder.append(DateTimeFormat.forPattern(format.substring(parseablePatternStart,i)))
-                     .appendTimeZoneShortName(timezones);
-        parseablePatternStart = i+1;
+      if (f == '\'') {
+        insideLiteral = !insideLiteral;
       }
-      if(f == 'Q' && !insideLiteral) {
-        formatBuilder.append(DateTimeFormat.forPattern(format.substring(parseablePatternStart,i)));
+      if (f == 'z' && !insideLiteral) {
+        formatBuilder.append(DateTimeFormat.forPattern(format.substring(parseablePatternStart, i)))
+                     .appendTimeZoneShortName(timezones);
+        parseablePatternStart = i + 1;
+      }
+      if (f == 'Q' && !insideLiteral) {
+        formatBuilder.append(DateTimeFormat.forPattern(format.substring(parseablePatternStart, i)));
         Pattern pattern = Pattern.compile("(GMT[+-]\\d{4})(.)(\\(?[A-Z]{1,5}\\)?)");
         Matcher matcher = pattern.matcher(input);
-        if(matcher.find()) {
+        if (matcher.find()) {
           formatBuilder.appendLiteral(matcher.group(3));
-        }
-        else {
+        } else {
           throw new FormattedException.Builder()
               .withErrorCode(FormattedException.ErrorCode.UNPARSABLE_TIMESTAMP)
-              .withMessage(String.format("Timestamp format has primitive Q but input did not contain GMT or UTC offset"
-                                         + "[%s]", format))
+              .withMessage(
+                  String.format(
+                      "Timestamp format has primitive Q but input did not contain GMT or UTC offset"
+                      + "[%s]", format
+                  )
+              )
               .build();
         }
-        parseablePatternStart = i+1;
+        parseablePatternStart = i + 1;
       }
     }
     formatBuilder.append(DateTimeFormat.forPattern(format.substring(parseablePatternStart)));
