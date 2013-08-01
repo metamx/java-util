666b668f1782276b93524c252c00d0da2440c970
fjy <fangjin@metamarkets.com>
2013-08-01 12:30:26 -0700

    add a resource closing yielder

---
+++ src/main/java/com/metamx/common/guava/ResourceClosingYielder.java
@@ -0,0 +1,43 @@
+package com.metamx.common.guava;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ */
+public class ResourceClosingYielder<OutType> implements Yielder<OutType>
+{
+  private final Yielder<OutType> baseYielder;
+  private final Closeable closeable;
+
+  public ResourceClosingYielder(Yielder<OutType> baseYielder, Closeable closeable)
+  {
+    this.baseYielder = baseYielder;
+    this.closeable = closeable;
+  }
+
+  @Override
+  public OutType get()
+  {
+    return baseYielder.get();
+  }
+
+  @Override
+  public Yielder<OutType> next(OutType initValue)
+  {
+    return new ResourceClosingYielder<OutType>(baseYielder.next(initValue), closeable);
+  }
+
+  @Override
+  public boolean isDone()
+  {
+    return baseYielder.isDone();
+  }
+
+  @Override
+  public void close() throws IOException
+  {
+    closeable.close();
+    baseYielder.close();
+  }
+}
