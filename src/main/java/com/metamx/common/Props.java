fea0503c7d72ffd8653b25ac1c591b92cba0f4d7
Maxim Logvinenko <mlogvinenko@gmail.com>
2019-05-30 13:46:06 +0200

    Load properties file from env vars

--- src/main/java/com/metamx/common/Props.java
+++ src/main/java/com/metamx/common/Props.java
@@ -18,6 +18,7 @@ package com.metamx.common;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Map;
 import java.util.Properties;
 
 public class Props {
@@ -25,6 +26,16 @@ public class Props {
   public static Properties fromFilename(String filename) throws IOException {
     final Properties props = new Properties();
     props.load(new FileInputStream(filename));
+    return props;
+  }
+
+  public static Properties fromEnv(String prefix) {
+    final Properties props = new Properties();
+    for (Map.Entry<String, String> env: System.getenv().entrySet()) {
+      if (env.getKey().startsWith(prefix)) {
+        props.put(env.getKey().toLowerCase().replaceAll("_", "."), env.getValue());
+      }
+    }
     return props;
   }
 
