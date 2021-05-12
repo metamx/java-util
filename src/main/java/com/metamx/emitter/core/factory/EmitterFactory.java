22eaa09534d350b8a11b7d6bb8950efe8f6e452d
Eugene Sevastianov <eugene.sevastianov@facetdata.com>
2021-05-12 17:25:34 +0300

    statsd metric/alert emitter

--- src/main/java/com/metamx/emitter/core/factory/EmitterFactory.java
+++ src/main/java/com/metamx/emitter/core/factory/EmitterFactory.java
@@ -5,15 +5,17 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.metamx.common.lifecycle.Lifecycle;
 import com.metamx.emitter.core.Emitter;
+import com.metamx.emitter.core.statsd.StatsDEmitterFactory;
 import org.asynchttpclient.AsyncHttpClient;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
 @JsonSubTypes(value = {
     @JsonSubTypes.Type(name = "http", value = HttpEmitterFactory.class),
+    @JsonSubTypes.Type(name = "statsd", value = StatsDEmitterFactory.class),
     @JsonSubTypes.Type(name = "logging", value = LoggingEmitterFactory.class),
     @JsonSubTypes.Type(name = "parametrized", value = ParametrizedUriEmitterFactory.class),
     @JsonSubTypes.Type(name = "composing", value = ComposingEmitterFactory.class),
-    @JsonSubTypes.Type(name = "noop", value = NoopEmiterFactory.class),
+    @JsonSubTypes.Type(name = "noop", value = NoopEmiterFactory.class)
 })
 public interface EmitterFactory
 {
---
+++ src/main/java/com/metamx/emitter/core/statsd/StatsDEmitterFactory.java
@@ -0,0 +1,53 @@
+package com.metamx.emitter.core.statsd;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.metamx.common.lifecycle.Lifecycle;
+import com.metamx.emitter.core.Emitter;
+import com.metamx.emitter.core.factory.EmitterFactory;
+import org.asynchttpclient.AsyncHttpClient;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class StatsDEmitterFactory extends StatsDEmitterConfig implements EmitterFactory
+{
+  @JsonCreator
+  public StatsDEmitterFactory(
+      @JsonProperty("hostname") String hostname,
+      @JsonProperty("port") Integer port,
+      @JsonProperty("prefix") @Nullable String prefix,
+      @JsonProperty("separator") @Nullable String separator,
+      @JsonProperty("includeHost") @Nullable Boolean includeHost,
+      @JsonProperty("dimensionMapPath") @Nullable String dimensionMapPath,
+      @JsonProperty("blankHolder") @Nullable String blankHolder,
+      @JsonProperty("dogstatsd") @Nullable Boolean dogstatsd,
+      @JsonProperty("dogstatsdConstantTags") @Nullable List<String> dogstatsdConstantTags,
+      @JsonProperty("dogstatsdServiceAsTag") @Nullable Boolean dogstatsdServiceAsTag,
+      @JsonProperty("dogstatsdEvents") @Nullable Boolean dogstatsdEvents
+  )
+  {
+    super(
+        hostname,
+        port,
+        prefix,
+        separator,
+        includeHost,
+        dimensionMapPath,
+        blankHolder,
+        dogstatsd,
+        dogstatsdConstantTags,
+        dogstatsdServiceAsTag,
+        dogstatsdEvents
+    );
+  }
+
+  @Override
+  public Emitter makeEmitter(ObjectMapper objectMapper, AsyncHttpClient httpClient, Lifecycle lifecycle)
+  {
+    Emitter retVal = StatsDEmitter.of(this, objectMapper);
+    lifecycle.addManagedInstance(retVal);
+    return retVal;
+  }
+}
